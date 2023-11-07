653c8a294409505fb4ed6e90234d84a0d73ca709
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-07 17:16:20 -0600

    Estilos al componente Paciente

--- src/components/Paciente.js
+++ src/components/Paciente.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 const Paciente = ({item}) => {
     const { paciente, fecha } = item
@@ -11,7 +11,7 @@ const Paciente = ({item}) => {
         const opciones = {
             weekday: 'long',
             year: 'numeric',
-            mont: 'long',
+            month: 'long',
             day: 'numeric'
         }
 
@@ -19,11 +19,41 @@ const Paciente = ({item}) => {
     }
 
     return (
-        <View>
-            <Text>{paciente}</Text>
-            <Text>{formatearFecha(fecha)}</Text>
+        <View style={styles.contenedor}>
+            <Text style={styles.label}>Paciente:</Text>
+            <Text style={styles.texto}>{paciente}</Text>
+            <Text style={styles.fecha}>{formatearFecha(fecha)}</Text>
         </View>
     )
-}
+};
+
+const styles = StyleSheet.create(
+    {
+        contenedor: {
+            backgroundColor: '#FFF',
+            padding: 20,
+            borderBottomColor: '#94a3B8',
+            borderBottomWidth: 1,
+        },
+
+        label: {
+            color: '#374151',
+            textTransform: 'uppercase',
+            fontWeight: '700',
+            marginBottom: 10,
+        },
+
+        texto: {
+            color: '#6D28D9',
+            fontSize: 20,
+            fontWeight: '700',
+            marginBottom: 10,
+        },
+
+        fecha: {
+            color:  '#374151',
+        },
+    }
+)
 
 export default Paciente

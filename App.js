63ebb419681a5762f882c5e39555a8e9702911e9
Omar Garcia <omar.garcia@trinnova.mx>
2023-11-08 16:17:03 -0600

    Estilos a la Vista del Paciente

--- App.js
+++ App.js
@@ -91,6 +91,7 @@ const App = () => {
       >
         <InformacionPaciente
           paciente={paciente}
+          setModalPaciente={setModalPaciente}
         />
       </Modal>
     </SafeAreaView>
--- src/components/InformacionPaciente.js
+++ src/components/InformacionPaciente.js
@@ -1,13 +1,65 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
+import { Pressable, SafeAreaView, Text, View, StyleSheet } from 'react-native'
 
-const InformacionPaciente = ({paciente}) => {
+const InformacionPaciente = ({paciente, setModalPaciente}) => {
     return (
-        <SafeAreaView>
-            <Text>Información Pacientes</Text>
+        <SafeAreaView
+            style={styles.contenedor}
+        >
+            <Text style={styles.titulo}>
+                Información {''}
+                <Text style={styles.tituloBold}>Paciente</Text>
+
+                    <View>
+                        <Pressable
+                            style={styles.btnCerrar}
+                            onPress={() => setModalPaciente(false)}
+                        >
+                            <Text style={styles.btnCerrarTexto}>X Cerrar</Text>
+                        </Pressable>
+                    </View>
+            </Text>
+
             <Text>{paciente.paciente}</Text>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create(
+    {
+        contenedor: {
+            backgroundColor: '#F59E0B',
+            flex: 1
+        },
+
+        titulo: {
+            fontSize: 30,
+            fontWeight: '600',
+            textAlign: 'center',
+            marginTop: 30,
+            color: '#FFF',
+        },
+
+        tituloBold: {
+            fontWeight: '900',
+        },
+
+        btnCerrar: {
+            marginVertical: 30,
+            backgroundColor: '#E06900',
+            marginHorizontal: 30,
+            padding: 15,
+            borderRadius: 10,
+        },
+
+        btnCerrarTexto: {
+            color: '#FFF',
+            textAlign: 'center',
+            fontWeight: '900',
+            fontSize: 16,
+            textTransform: 'uppercase',
+        },
+    }
+)
 
 export default InformacionPaciente

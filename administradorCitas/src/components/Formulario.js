29f8873eab3a83aa9b2febe8b2af210eb4575f4d
Omar Garcia <omar.garcia@morsa.com.mx>
2025-03-15 17:58:01 -0600

    up

---
+++ administradorCitas/src/components/Formulario.js
@@ -0,0 +1,278 @@
+// Atajo: rafce
+import React, {useState, useEffect} from 'react';
+
+import {
+    Modal,
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    TextInput,
+    View,
+    ScrollView,
+    Pressable,
+    Alert
+} from 'react-native';
+
+import DatePicker from 'react-native-date-picker';
+
+const Formulario = ({modalVisible, setModalVisible, pacientes, setPacientes, paciente: pacienteObj}) => {
+    const [nombrePaciente, setNombrePaciente] = useState('');
+    const [id, setId] = useState('');
+    const [nombrePropietario, setNombrePropietario] = useState('');
+    const [emailPropietario, setEmailPropietario] = useState('');
+    const [telefonoPropietario, setTelefonoPropietario] = useState('');
+    const [fecha, setFecha] = useState(new Date());
+    const [sintomasPaciente, setSintomasPaciente] = useState('');
+
+    
+    /**
+     * [] = se ejecuta 1 sola vez
+    */
+    useEffect(() => {
+        if (Object.keys(pacienteObj).length > 0) {
+            console.log(('Hay algo'));
+
+            setId(pacienteObj.id);
+            setNombrePaciente(pacienteObj.nombrePaciente);
+            setNombrePropietario(pacienteObj.nombrePropietario);
+            setEmailPropietario(pacienteObj.emailPropietario);
+            setTelefonoPropietario(pacienteObj.telefonoPropietario);
+            setFecha(pacienteObj.fecha);
+            setSintomasPaciente(pacienteObj.sintomasPaciente);
+        }
+
+        console.log('Formulario Listo...');
+        console.log(pacienteObj);
+    }, []);
+
+    const handleCita = () => {
+        // console.log('Agregar cita');
+        if ( [nombrePaciente, nombrePropietario, emailPropietario, fecha, sintomasPaciente].includes('') ) {
+            // console.log('Error');
+            Alert.alert(
+                'Error',
+                'Todos los campos son obligatorios',
+                // 3er campo....
+                // [
+                //     {
+                //         text: 'Recordarme después'
+                //     },
+                //     {
+                //         text: 'Cancelar',
+                //         style: 'cancel' //default - destructive
+                //     },
+                //     {
+                //         text: 'OK'
+                //     }
+                // ]
+            );
+
+            return;
+        }
+
+        const nuevoPaciente = {
+            id: Date.now(),
+            nombrePaciente,
+            nombrePropietario,
+            emailPropietario,
+            telefonoPropietario,
+            fecha,
+            sintomasPaciente
+        };
+        // console.log(nuevoPaciente);
+        setPacientes( [...pacientes, nuevoPaciente] );
+        setModalVisible(!modalVisible);
+        setNombrePaciente('');
+        setNombrePropietario('');
+        setEmailPropietario('');
+        setTelefonoPropietario('');
+        setFecha(new Date());
+        setSintomasPaciente('');
+    }
+
+    return (
+        <Modal
+            animationType='slide'
+            visible={modalVisible}
+        >
+            <SafeAreaView style={styles.contenido}>
+                <ScrollView>
+                    <Text style={styles.titulo}>
+                        Nueva {''}
+                        <Text style={styles.tituloBold}>Cita</Text>
+                    </Text>
+
+                    <Pressable 
+                        style={styles.btnCancelar}
+                        onLongPress={ () => setModalVisible(!modalVisible) }
+                    >
+                        <Text style={styles.btnCancelarTexto}>X Cancelar</Text>
+                    </Pressable>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Nombre Paciente</Text>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder='Nombre Paciente'
+                            placeholderTextColor={'#666'}
+                            value={nombrePaciente}
+                            onChangeText={setNombrePaciente}
+                        />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Nombre Propietario</Text>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder='Nombre Propietario'
+                            placeholderTextColor={'#666'}
+                            value={nombrePropietario}
+                            onChangeText={setNombrePropietario}
+                        />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Email Propietario</Text>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder='Email Propietario'
+                            placeholderTextColor={'#666'}
+                            keyboardType='email-address'
+                            value={emailPropietario}
+                            onChangeText={setEmailPropietario}
+                        />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Teléfono Propietario</Text>
+                        <TextInput 
+                            style={styles.input}
+                            placeholder='Teléfono Propietario'
+                            placeholderTextColor={'#666'}
+                            keyboardType='phone-pad'
+                            value={telefonoPropietario}
+                            onChangeText={setTelefonoPropietario}
+                            maxLength={10}
+                        />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Fecha Alta</Text>
+                        
+                        <View style={styles.fechaContenedor}>
+                            <DatePicker 
+                                date={fecha}
+                                locale='es'
+                                onDateChange={ (date) => setFecha(date) }
+                            />
+                        </View>
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Síntomas</Text>
+                        <TextInput 
+                            style={[styles.input, styles.sintomasInput]}
+                            placeholder='Síntomas Paciente'
+                            placeholderTextColor={'#666'}
+                            value={sintomasPaciente}
+                            onChangeText={setSintomasPaciente}
+                            multiline={true}
+                            numberOfLines={4}
+                        />
+                    </View>
+
+                    <Pressable 
+                        style={styles.btnNuevaCita}
+                        onPress={handleCita}
+                    >
+                        <Text style={styles.btnNuevaCitaTexto}>Agregar Paciente</Text>
+                    </Pressable>
+
+                </ScrollView>
+            </SafeAreaView>
+        </Modal>
+    )
+};
+
+const styles = StyleSheet.create(
+    {
+        contenido: {
+            backgroundColor: '#6D28D9',
+            flex: 1,
+        },
+
+        titulo: {
+            fontSize: 30,
+            fontWeight: '600',
+            textAlign: 'center',
+            marginTop: 30,
+            color: '#FFF',
+        },
+
+        tituloBold: {
+            fontWeight: '900',
+        },
+
+        btnCancelar: {
+            marginVertical: 30,
+            backgroundColor: '#5827A4',
+            marginHorizontal: 30,
+            padding: 15,
+            borderRadius: 10
+        },
+
+        btnCancelarTexto: {
+            color: '#FFF',
+            textAlign: 'center',
+            fontWeight: '900',
+            fontSize: 16,
+            textTransform: 'uppercase',
+        },
+
+        campo: {
+            marginTop: 10,
+            marginHorizontal: 30,
+        },
+
+        label: {
+            color: '#FFF',
+            marginBottom: 10,
+            marginTop: 15,
+            fontSize: 20,
+            fontWeight: '600',
+        },
+
+        input: {
+            backgroundColor: '#FFF',
+            padding: 15,
+            borderRadius: 10,
+        },
+
+        sintomasInput: {
+            height: 100
+        },
+
+        fechaContenedor: {
+            backgroundColor: '#FFF',
+            borderRadius: 10,
+        },
+
+        btnNuevaCita: {
+            marginVertical: 50,
+            backgroundColor: '#F59E0B',
+            paddingVertical: 15,
+            marginHorizontal: 30,
+            borderRadius: 10
+        },  
+
+        btnNuevaCitaTexto: {
+            textAlign: 'center',
+            color: '#5827A4',
+            textTransform: 'uppercase',
+            fontWeight: '900',
+            fontSize: 16
+        }
+    }
+);
+
+export default Formulario;
---
+++ administradorCitas/src/components/Paciente.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import {Text, View, StyleSheet, Pressable} from 'react-native';
+
+const Paciente = (  {item, setModalVisible, pacienteEditar} ) => {
+    // console.log(item);
+    const { nombrePaciente, fecha, id } = item;
+
+    const formatearFEcha = fecha => {
+        const nuevaFecha = new Date(fecha);
+
+        const opciones = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+
+        return nuevaFecha.toLocaleDateString('es-ES', opciones);
+    };
+
+    return (
+        <View style={styles.contenedor}>
+            <Text style={styles.label}>Paciente:</Text>
+            <Text style={styles.texto}> {nombrePaciente} </Text>
+            <Text style={styles.fecha}> {formatearFEcha(fecha)} </Text>
+
+            <View style={styles.contenedorBotones}>
+                <Pressable 
+                    style={ [styles.btn, styles.btnEditar] }
+                    onLongPress={ () => {
+                            setModalVisible(true)
+                            pacienteEditar(id)
+                        } 
+                    }
+                >
+                    <Text style={styles.btnTexto}>Editar</Text>
+                </Pressable>
+
+                <Pressable style={ [styles.btn, styles.btnEliminar] }>
+                    <Text style={styles.btnTexto}>Eliminar</Text>
+                </Pressable>
+            </View>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create(
+    {
+        contenedor: {
+            backgroundColor: '#FFF',
+            padding: 20,
+            borderBottomColor: '#94A3B8',
+            borderBottomWidth: 1
+        },
+
+        label: {
+            color: '#374151',
+            textTransform: 'uppercase',
+            fontWeight: '700',
+            marginBottom: 10
+        },
+
+        texto: {
+            color: '#6D28D9',
+            fontSize: 20,
+            fontWeight: '700',
+            marginBottom: 10
+        },
+
+        fecha: {
+            color: '#374151'
+        },
+
+        contenedorBotones: {
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginTop: 20
+        },
+
+        btn: {
+            paddingVertical: 5,
+            paddingHorizontal: 20,
+            borderRadius: 5
+
+        },
+
+        btnEditar: {
+            backgroundColor: '#F59E0B'
+        },
+
+        btnEliminar: {
+            backgroundColor: '#EF4444'
+        },
+
+        btnTexto: {
+            color: '#FFF',
+            textTransform: 'uppercase',
+            fontWeight: '700',
+            fontSize: 12
+        }
+    }
+);
+
+export default Paciente;
